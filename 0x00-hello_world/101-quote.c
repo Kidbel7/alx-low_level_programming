64c6098953eb7161b7c856ddf56874d1d1c114bf
Kidbel7 <kidtmariam@gmail.com>
2022-06-02 14:44:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 /**
  * main - prints "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- *   followed by a new line, to the standard error.
+ * followed by a new line, to the standard error.
  *   
- *   Return: Always (1) (Success)
+ * Return: Always (1) (Success)
  */
 int main(void)
 {
