ce68ba9d987a83e89182ab346a2f9ea2fb40f42d
Kidbel7 <kidtmariam@gmail.com>
2022-06-03 09:07:38 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,11 @@ int main(void)
 {
 	char alph;
 
-	for (alph = 'a'; alph <= 'z'; alph++; alph != q; alph != e;)
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
 		putchar(alph);
+	}
 	putchar('\n');
 	return (0);
 }
