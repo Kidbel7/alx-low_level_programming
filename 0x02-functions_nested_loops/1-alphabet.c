8bcbc257e198af3d06b4d96a66ad85477cc2a085
Kidbel7 <kidtmariam@gmail.com>
2022-06-07 08:00:05 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * pritn_alphabet - prints the alphabet, in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
