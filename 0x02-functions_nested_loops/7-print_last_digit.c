faeb6d5b7935a6b399865a5cfaf5c49d46cac7cc
Kidbel7 <kidtmariam@gmail.com>
2022-06-07 13:05:47 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit 
+ * print_last_digit - prints the last digit
  * @n : number to be checked
  * Return: value of last digit
  */
