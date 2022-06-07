53a65c36762883bf442da50f8802d0879202d977
Kidbel7 <kidtmariam@gmail.com>
2022-06-07 11:51:21 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,7 @@
 /**
  * _isalpha - checks for alphabet character
  * @c - character to be checked
- * Return: 1 if c is a letter, lowercase or uppercase
- * Return: 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
