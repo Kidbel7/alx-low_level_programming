3c6be8e8987e3ce6870c6b2219cdbb5a7f6270c8
Kidbel7 <kidtmariam@gmail.com>
2022-06-09 06:28:28 -0700

    check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for upper case character
+ * @c: character to be checked
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	char c;
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
