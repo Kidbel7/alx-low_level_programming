b708ee6965387a3242add00007ee2ff7dbac3524
Kidbel7 <kidtmariam@gmail.com>
2022-06-09 11:59:34 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9
- * Return: 0
  */
 
 void print_numbers(void)
@@ -10,7 +9,8 @@ void print_numbers(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
+	{
 		_putchar(c);
-		_putchar('\n');
-		return (0);
+	}
+	_putchar('\n');
 }
