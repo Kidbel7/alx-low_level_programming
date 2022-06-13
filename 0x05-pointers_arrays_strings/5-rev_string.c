f8576ed6de4eb043c63ec71a6c492ba64800d6c1
Kidbel7 <kidtmariam@gmail.com>
2022-06-13 05:46:07 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,19 @@
 
 void rev_string(char *s)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, k = 0;
 	char c;
 
-	while (s[j++])
-		i++;
-	for (j = i -1; j >= i / 2; j++)
+	while (s[i] != '\0')
 	{
-		c = s[j];
-		s[j] = s[i - j - 1];
-		s[i - j - 1] = c;
+		i++;
+	}
+	j = i - 1;
+
+	for (k < i / 2; k++)
+	{
+		c = s[k];
+		s[k] = s[j];
+		s[j--] = c;
 	}
 }
