67ad6e31f98aaf5ad9ecb0c86aaddc5ab28e6007
Kidbel7 <kidtmariam@gmail.com>
2022-06-13 07:31:26 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,13 @@ int _atoi(char *s)
 	int i = 1;
 	unsigned int j = 0;
 
-	do{
+	do {
 		if (*s == '-')
 			i *= -1;
 		else if (*s >= '0' && *s <= '9')
 			j = (j * 10) + (*s - '0');
 		else if (j > 0)
 			break;
-	}while (*s++);
+	} while (*s++);
 	return (i * j);
 }
