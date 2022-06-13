73e11a10c3569a8ff0f72bfd62c67f71b99f096c
Kidbel7 <kidtmariam@gmail.com>
2022-06-13 06:19:40 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ */
+
+void pust_half(char *str)
+{
+	int i, j, k;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	k = i;
+	j = k / 2;
+	while (j <= k)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
