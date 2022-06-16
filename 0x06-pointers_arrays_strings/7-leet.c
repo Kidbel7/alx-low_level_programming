91393002e8d9eb6d1144bcf1ec123093c424d30f
Kidbel7 <kidtmariam@gmail.com>
2022-06-16 01:21:54 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,13 +12,13 @@ char *leet(char *s)
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == a[j])
+			if (s[i] == a[j])
 			{
-				n[i] = b[j];
+				s[i] = b[j];
 			}
 		}
 	}
