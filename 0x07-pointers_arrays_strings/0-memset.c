3a580735ef28f9eced2c677ea609daf262a043d2
Kidbel7 <kidtmariam@gmail.com>
2022-06-20 06:29:13 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
-
-	while (n > 0)
-	{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
 		s[i] = b;
-		i++;
-		n--;
 	}
 	return (s);
 }
