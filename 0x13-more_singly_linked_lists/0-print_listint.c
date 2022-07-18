606632c70d7414256912a91fa2535f1e76dbc0a2
Kidbel7 <kidtmariam@gmail.com>
2022-07-18 06:48:11 -0700

    free

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a `listint_t` list
+ * @h: singly linked list
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes;
+
+	for (nodes = 0; h; nodes++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a `listint_t` list
+ * @head: head of list
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}
