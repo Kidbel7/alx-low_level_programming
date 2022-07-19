03d3adff69a6c2dc8addaf1363697e22075df0e5
Kidbel7 <kidtmariam@gmail.com>
2022-07-19 02:51:17 -0700

    reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,6 +8,19 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev;
+	listint_t *next;
+
+	prev = NULL;
+	next = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
 }
