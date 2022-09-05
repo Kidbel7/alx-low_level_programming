79acf8d2036006083c95a9409ac71b4cc8a2a820
Kidbel7 <kidtmariam@gmail.com>
2022-09-05 16:43:33 -0700

    fix

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int add(int a, int *b)
+int add(int a, int b)
 {
-	int i;
-	int sum = 0;
-
-	for (i = 0; i < a; i++)
-	{
-		sum += b[i];
-	}
-	return sum;
+	return(a + b);
+}
+int sub(int a, int b)
+{
+	return(a - b);
+}
+int mul(int a, int b)
+{
+	return(a * b);
+}
+int div(int a, int b)
+{
+	return(a / b);
+}
+int mod(int a, int b)
+{
+	return(a % b);
 }
