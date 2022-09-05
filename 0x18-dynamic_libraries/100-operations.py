3134a6586e958346b579f3e22442a14787a03a48
Kidbel7 <kidtmariam@gmail.com>
2022-09-05 16:51:06 -0700

    test

--- 0x18-dynamic_libraries/100-operations.py
+++ 0x18-dynamic_libraries/100-operations.py
@@ -1,11 +1,11 @@
+import random
 import ctypes
 
-_sum = ctypes.CDLL('100-operations.so')
-_sum.add.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_int))
-
-def add(b):
-    global _sum
-    a = len(b)
-    array_type = ctypes.c_int * a
-    result = _sum.add(ctypes.c_int(a), array_type(*b))
-    return int(result)
+cops = ctypes.CDLL('./100-operations.so')
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
