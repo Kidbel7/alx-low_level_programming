8e3cde4c9a21ec4798683ff68e1887dc0b0e59b4
Kidbel7 <kidtmariam@gmail.com>
2022-09-05 18:03:57 -0700

    add

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Kidbel7/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gigamil.so
+wget -P /tmp/ https://github.com/Kidbel7/alx-low_level_programming/blob/96c649d70f6d4d20f3a5efe241038ddaed02e11a/0x18-dynamic_libraries/gigamil.so
 export LD_PRELOAD=/tmp/gigamil.so
